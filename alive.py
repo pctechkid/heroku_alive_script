daf77fc5d32ec88b3e0acadbea33d7f4850a2e6e
krishna karanam <karanam.s@husky.neu.edu>
2019-04-20 16:47:04 -0700

    Update alive.py
    handled retry

--- alive.py
+++ alive.py
@@ -6,7 +6,10 @@ links = ["https://krishnakaranam.herokuapp.com/"]
 for each in links:
     try:
         page = requests.get(each)
-        print(page.status_code)
+        print("First try:", page.status_code)
+        if page.status_code != 200:
+            page = requests.get(each)
+            print("retry:", page.status_code)
     except Exception as e:
         print(e)
         print("tried link: ", each)
